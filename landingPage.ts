5d356aa92de91bd365e8d1f98fd9b361b5394242
Boran Kuşcu <boran7200@gmail.com>
2023-10-02 14:50:56 +0300

    post repost added

--- landingPage.ts
+++ landingPage.ts
@@ -2,26 +2,25 @@ import { login } from "./login";
 import MapView from "@arcgis/core/views/MapView.js";
 import esriConfig from "@arcgis/core/config.js";
 import Map from "@arcgis/core/Map.js";
-import { User } from "./types";
+import { User, userReport } from "./types";
 import { apiConnection } from "./apiConnectionService";
 import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer.js"
 import Graphic from "@arcgis/core/Graphic.js";
 import PopupTemplate from "@arcgis/core/PopupTemplate.js";
 import Popup from "@arcgis/core/widgets/Popup.js";
-import { inputPop, xputPop } from "./inputPopupContent";
+import { inputPop, inputButton,inputDetails,inputTitle } from "./inputPopupContent";
 import Geometry from "@arcgis/core/geometry/Geometry";
+
 
 let viewDiv= document.getElementById("viewDiv");
 viewDiv!.appendChild(login);
-
-//document.body.appendChild(login);
 
 export async function changePage(user :User){
     
     var graphics = await apiConnection.getPreviousReports(user);
     
     viewDiv!.removeChild(login);
-    esriConfig.apiKey = "Your_API_key";
+    esriConfig.apiKey = "AAPK52342f431d2440c7ae7bf9b7de0d0b86R9JtgalY2YgKIDa8rsG4uO6Z3HzLNLpUPoXFeSjk0VX5QNWh5DFZGx-hNGZRMLBH";
     const map = new Map({
         basemap: "arcgis-topographic" // Basemap layer service
         });
@@ -36,13 +35,15 @@ export async function changePage(user :User){
     var graphicsLayer = new GraphicsLayer();  
     map.layers.add(graphicsLayer);  
     graphicsLayer.addMany(graphics);
-
+    
 
     var temp = document.createElement("div");
     var button = document.createElement("button");
     button.innerHTML="Add report";
     temp.appendChild(button);
     view.ui.add(temp,"top-right");
+
+
 
     var edit=false;
 
@@ -74,6 +75,24 @@ export async function changePage(user :User){
                     content:[inputPop]
                 }
             });
+            inputButton.addEventListener("click",()=>{
+                graphic.attributes={title:(inputTitle.value),
+                details:(inputDetails.value)};
+                console.log(event.mapPoint.latitude,event.mapPoint.longitude);
+                graphic.popupTemplate={
+                    title:"{title}",
+                    content:[{
+                        type: "text",
+                        text: "{details}"
+                }]
+                }
+                apiConnection.postUserReport({userId:user.id,
+                    reportTitle: graphic.attributes.title,reportDescription:graphic.attributes.details,
+                    x:clickedPoint.longitude, y:clickedPoint.latitude },user.token);
+                view.popup.visible=false;
+            })
+            inputTitle.value = '';
+            inputDetails.value='';
             graphicsLayer.add(graphic);
             edit=false;
         }
